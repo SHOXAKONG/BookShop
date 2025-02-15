0adf72e3dd1e63497ce29482a521e124c4e4ac67
Shohruh <bekmurodovshohruh0224@gmail.com>
2025-02-15 19:50:49 +0500

    First Commit

--- library/migrations/0001_initial.py
+++ library/migrations/0001_initial.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
             name='Book',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('cover_image', models.ImageField(upload_to='images/')),
+                ('cover_image', models.ImageField(upload_to='media/')),
                 ('title', models.CharField(max_length=200)),
                 ('price', models.DecimalField(decimal_places=2, max_digits=5)),
                 ('count', models.IntegerField(default=1)),
